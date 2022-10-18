fc13f280efc84003c288ec067dd6f9788dfdf323
PavanSingerReddy <kandulapavansingerreddy@gmail.com>
2022-10-19 01:47:30 +0530

    cleaning code

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express"
 import axios from "axios"
 
-import {parse} from "node-html-parser"
+import { parse } from "node-html-parser"
 
-if(process.env.NODE_ENV!=="production"){
+//below two lines of code is for using both import and require
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+
+if (process.env.NODE_ENV !== "production") {
 
     const dotenv = require('dotenv')
-    
+
     dotenv.config()
 
 }
@@ -17,41 +22,57 @@ const port = process.env.PORT
 
 
 
-app.use(express.urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended: true }))
 
 
 
+app.get('/',(req,res)=>{
 
-app.post('/:apikey',async (req,res)=>{
+    res.send("do post request using api key")
+
+})
+
+
+app.post('/:apikey', async (req, res) => {
 
     const apikey = req.params.apikey
 
-    console.log(`api key is :${apikey}`)
-    console.log(`${process.env.APIKEY}`)
-    if(apikey === process.env.APIKEY){
+    if (apikey === process.env.APIKEY) {
+
+
+        try {
+
+
+            let url = req.body.url
+    
+            let data = {}
+    
+            const response = await doCORSRequest(url)
+            const root = parse(response)
+            const meta = root.querySelectorAll('meta[property^="og"]');
+    
+            for (let i = 0; i < meta.length; i++) {
+    
+    
+                data[`${meta[i].getAttribute("property").replace("og:", "")}`] = `${meta[i].getAttribute("content")}`
+    
+            }
+    
+    
+            res.json(data)
+            
+
+        } catch (error) {
+            
+            console.log(error)
+
+            res.status(400).send(error)
+
+        }
 
 
 
-    let url = req.body.url
-
-    let data = {}
-
-    const response = await doCORSRequest(url)
-    const root = parse(response)
-    const meta = root.querySelectorAll('meta[property^="og"]');
-
-    for(let i=0;i<meta.length;i++){
-        
-
-        data[`${meta[i].getAttribute("property").replace("og:","")}`] = `${meta[i].getAttribute("content")}`
-
-    }
-
-
-    res.json(data)
-
-
-    } else{
+    } else {
 
         res.status(404).send('Invalid API Key');
     }
@@ -72,7 +93,7 @@ async function doCORSRequest(url) {
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`listening on port : ${port}`)
 })
 
